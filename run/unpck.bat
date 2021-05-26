a40b9bf2ffd79a68c377c9b44da7bb45642c0d58
Your Name <you@example.com>
2021-05-26 14:03:57 +0930

    z

---
+++ run/unpck.bat
@@ -0,0 +1,26 @@
+@echo off 
+setlocal
+REM usage split.bat <filename>
+set filename=%~n1
+set extension=%~x1
+::endlocal 
+
+set filen=%filename:~0,-4%
+set exten=%filename:~-4%
+set extt=%exten%%extension%
+::echo input file: ^<%filename%^> ^<%extension%^> ^<%filen%^> ^<%exten%^> ^<%extt%^>
+
+if NOT %extt% == .fcl.bz2 goto bad
+
+bzip2 -d -k %filen%.fcl.bz2
+chmod 0777 %filen%.fcl
+fcl extr %filen%.fcl
+chmod -R 0777 %filen%
+
+if exist %filen% rm -f %filen%.fcl
+
+::echo ok
+exit
+
+:bad
+echo Bad extension, expect ^<file^>.fcl.bz2
