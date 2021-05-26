5c22dbb83fe913286b86de2fe2820cca3d470ecd
Your Name <you@example.com>
2021-05-26 13:57:05 +0930

    z

---
+++ msys2_init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# https://repo.msys2.org/distrib/x86_64/msys2-x86_64-20200903.exe
+
+#pacman -Syu --noconfirm
+pacman -S --noconfirm git
+pacman -S --noconfirm mingw-w64-x86_64-gcc
+pacman -S --noconfirm mingw-w64-x86_64-cmake
+pacman -S --noconfirm autoconf
+pacman -S --noconfirm make
+pacman -S --noconfirm tar
+pacman -S --noconfirm lzip
+pacman -S --noconfirm dos2unix
+
+# need for mpir
+# pacman -S mingw-w64-x86_64-yasm
